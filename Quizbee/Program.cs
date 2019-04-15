939beeb30a0d0bf235e6607c844d581980c3db3f
Muhammad Aosaf <aosafpak@gmail.com>
2019-04-15 20:29:53 +0500

    Login form added and form 1 deleted

--- Quizbee/Program.cs
+++ Quizbee/Program.cs
@@ -16,7 +16,7 @@ namespace Quizbee
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
